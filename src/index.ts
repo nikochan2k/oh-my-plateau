ba3fa9c6d7e1b529b9d0b880f4b746fde513a9fb
Yoshihiro Seki <nikochan2k@gmail.com>
2021-09-20 15:59:18 +0900

    fix bugs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
 import { readFileSync, writeFileSync } from "fs";
-import { glob } from "glob";
 import { extname } from "path";
 import { Command } from "commander";
+
+const COORDINATES = /^\s*-?\d+(\.\d+)?(\s+-?\d+(\.\d+))*\s*$/;
+const ALL_SPACES = /^\s+$/;
+
 const program = new Command("oh-my-plateau");
 program
   .usage("[options] <filePattern>")
@@ -20,32 +23,21 @@ const options = program.opts();
 
 const EasySax = require("easysax");
 
-const pattern = program.args[0];
-glob(pattern, (err, matches) => {
-  if (err) {
-    console.warn("Illegal glob file pattern: " + pattern);
-    process.exit(1);
-  }
-
-  for (let path of matches) {
-    try {
-      const xml = readFileSync(path, "utf-8");
-      const corrected = parse(xml);
-      if (!options.overwrite) {
-        const extension = extname(path);
-        const withoutExt = path.substr(0, path.length - extension.length);
-        path = withoutExt + "_mod" + extension;
-      }
-      writeFileSync(path, corrected, "utf-8");
-      console.log('Processed "' + path + '"');
-    } catch (e) {
-      console.log('Error "' + path + '":\n' + e);
+for (let path of program.args) {
+  try {
+    const xml = readFileSync(path, "utf-8");
+    const corrected = parse(xml);
+    if (!options.overwrite) {
+      const extension = extname(path);
+      const withoutExt = path.substr(0, path.length - extension.length);
+      path = withoutExt + "_mod" + extension;
     }
+    writeFileSync(path, corrected, "utf-8");
+    console.log('Processed "' + path + '"');
+  } catch (e) {
+    console.log('Error "' + path + '":\n' + e);
   }
-});
-
-const COORDINATES = /^\s*-?\d+(\.\d+)?(\s+-?\d+(\.\d+))*\s*$/;
-const ALL_SPACES = /^\s+$/;
+}
 
 function parse(xml: string) {
   const parser = new EasySax();
@@ -67,14 +59,16 @@ function parse(xml: string) {
     if (COORDINATES.test(text)) {
       const coords = text.trim().split(/\s+/);
       for (let i = 0, end = coords.length; i + 1 < end; i += 3) {
-        const lng = parseFloat(coords[i]);
-        const lat = parseFloat(coords[i + 1]);
+        const lngStr = coords[i];
+        const lng = parseFloat(lngStr);
+        const latStr = coords[i + 1];
+        const lat = parseFloat(latStr);
         if (isNaN(lng) || isNaN(lng)) {
           continue;
         }
         if (20 <= lng && lng <= 46 && 122 <= lat && lat <= 154) {
-          coords[i] = lat.toString();
-          coords[i + 1] = lng.toString();
+          coords[i] = latStr;
+          coords[i + 1] = lngStr;
         }
       }
       chunks.push(coords.join(" "));
